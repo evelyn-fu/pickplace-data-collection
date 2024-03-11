df14d42f38d6818b1a974e13c7860706f51fca32
evelynfu <evelynfu@mit.edu>
2024-03-11 16:02:20 -0400

    script to save realsense images

---
+++ scripts/realsense.py
@@ -0,0 +1,107 @@
+import pyrealsense2 as rs
+import numpy as np
+from PIL import Image
+import argparse
+import time
+import os
+
+
+def realsense(dirstr="temp"):
+    if not os.path.exists(dirstr):
+        os.makedirs(dirstr)
+    if not os.path.exists(dirstr+"/rgb/"):
+        os.makedirs(dirstr+"/rgb/")
+    if not os.path.exists(dirstr+"/depth/"):
+        os.makedirs(dirstr+"/depth/")
+
+    # Create a pipeline
+    pipeline = rs.pipeline()
+
+    # Create a config and configure the pipeline to stream
+    #  different resolutions of color and depth streams
+    config = rs.config()
+
+    # Get device product line for setting a supporting resolution
+    pipeline_wrapper = rs.pipeline_wrapper(pipeline)
+    pipeline_profile = config.resolve(pipeline_wrapper)
+    device = pipeline_profile.get_device()
+    device_product_line = str(device.get_info(rs.camera_info.product_line))
+
+    found_rgb = False
+    for s in device.sensors:
+        if s.get_info(rs.camera_info.name) == 'RGB Camera':
+            found_rgb = True
+            break
+    if not found_rgb:
+        print("The demo requires Depth camera with Color sensor")
+        exit(0)
+
+    config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+
+    if device_product_line == 'L500':
+        config.enable_stream(rs.stream.color, 960, 540, rs.format.bgr8, 30)
+    else:
+        config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+
+    # Start streaming
+    profile = pipeline.start(config)
+
+    # Getting the depth sensor's depth scale (see rs-align example for explanation)
+    depth_sensor = profile.get_device().first_depth_sensor()
+    depth_scale = depth_sensor.get_depth_scale()
+    print("Depth Scale is: " , depth_scale)
+
+    # We will be removing the background of objects more than
+    #  clipping_distance_in_meters meters away
+    clipping_distance_in_meters = 3 # meters
+    clipping_distance = clipping_distance_in_meters / depth_scale
+
+    # Create an align object
+    # rs.align allows us to perform alignment of depth frames to others frames
+    # The "align_to" is the stream type to which we plan to align depth frames.
+    align_to = rs.stream.color
+    align = rs.align(align_to)
+
+    # Streaming loop
+    start_t = time.time()
+    try:
+        while True:
+            time_ms = int((time.time() - start_t) * 1000)
+            timestr = f"{time_ms:06d}"
+            # Get frameset of color and depth
+            frames = pipeline.wait_for_frames()
+            # frames.get_depth_frame() is a 640x360 depth image
+
+            # Align the depth frame to color frame
+            aligned_frames = align.process(frames)
+
+            # Get aligned frames
+            aligned_depth_frame = aligned_frames.get_depth_frame() # aligned_depth_frame is a 640x480 depth image
+            color_frame = aligned_frames.get_color_frame()
+
+            # Validate that both frames are valid
+            if not aligned_depth_frame or not color_frame:
+                continue
+
+            depth_image = np.asanyarray(aligned_depth_frame.get_data())
+            color_image = np.asanyarray(color_frame.get_data())
+
+            color_pil = Image.fromarray(color_image)
+            color_pil.save(dirstr+"/rgb/"+timestr+".png")
+
+            depth_pil = Image.fromarray(depth_image)
+            depth_pil.save(dirstr+"/depth/"+timestr+".png")
+    finally:
+        pipeline.stop()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "save_dir",
+        default="temp",
+        help="directory to save images in",
+        nargs='?',
+    )
+    args = parser.parse_args()
+    save_dir_path = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'tests_realsense', args.save_dir))
+    realsense(save_dir_path)
