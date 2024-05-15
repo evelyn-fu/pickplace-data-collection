6c1423eb6565024670c107f70b4716829d2aa6c4
evelynfu <evelynfu@mit.edu>
2024-05-15 14:58:27 -0400

    finish integrating hardware camera drivers first draft

---
+++ random/get_realsense_intrinsics.py
@@ -0,0 +1,38 @@
+import pyrealsense2 as rs
+import numpy as np
+
+if __name__ == "__main__":
+    # Create a pipeline
+    pipeline = rs.pipeline()
+
+    # Create a config and configure the pipeline to stream
+    #  different resolutions of color and depth streams
+    config = rs.config()
+
+    profile = pipeline.start(config)
+    intr = profile.get_stream(rs.stream.color).as_video_stream_profile().get_intrinsics()
+    w = intr.width
+    h = intr.height
+    ppx = intr.ppx
+    ppy = intr.ppy
+    fx = intr.fx
+    fy = intr.fy
+    K = np.array([[fx, 0.0, ppx],
+                    [0.0, fy, ppy],
+                    [0.0, 0.0, 1.0]])
+    
+    print("rgb intrinsics", K)
+
+    intr = profile.get_stream(rs.stream.depth).as_video_stream_profile().get_intrinsics()
+    w = intr.width
+    h = intr.height
+    ppx = intr.ppx
+    ppy = intr.ppy
+    fx = intr.fx
+    fy = intr.fy
+    K = np.array([[fx, 0.0, ppx],
+                    [0.0, fy, ppy],
+                    [0.0, 0.0, 1.0]])
+    
+    print("depth intrinsics", K)
+    pipeline.stop()
