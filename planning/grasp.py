Fix flattened principle axis computation

planning/grasp.py
@@ -289,7 +289,7 @@ class GraspListener():
 
                     viz_geoms = [manipuland_cloud, gripper_cloud, gripper_next_cloud]
                     o3d.visualization.draw_plotly(viz_geoms)
-                    input(f"Found grasp {last_signed_distance}, {signed_distance}, {X_WGlast}")
+                    # input(f"Found grasp {last_signed_distance}, {signed_distance}, {X_WGlast}")
                 return last_signed_distance, X_WGlast
             
             # Record the computed values using last z.
@@ -325,7 +325,7 @@ class GraspListener():
 
                         viz_geoms = [manipuland_cloud, gripper_cloud, gripper_next_cloud]
                         o3d.visualization.draw_plotly(viz_geoms)
-                        input(f"Found grasp going backwards {last_signed_distance}, {signed_distance}, {X_WGlast}")
+                        # input(f"Found grasp going backwards {last_signed_distance}, {signed_distance}, {X_WGlast}")
                     return signed_distance, X_WGnew
                 
                 # Record the computed values using last z.
@@ -1259,7 +1259,7 @@ class GraspListener():
                         # min_range=-0.11 - buffer,
                         # max_range=-0.01 - buffer,
                         use_extra_buffer=use_extra_buffer,
-                        viz=True
+                        viz=False
                     )
                     if np.isnan(distance):
                         print(f"Frame {i} in collision")

planning/two_grasp_display_planner.py
@@ -1255,13 +1255,15 @@ class TwoGraspPlanner(LeafSystem):
         )
         self.meshcat.SetObject("cropped_cloud", cropped_cloud, point_size=0.001, rgba=Rgba(0,0,1,1))
 
+        # Flatten cloud to determine what way to place object after bin picking. We need to place it in a
+        # way such that good scanning grasps are kinematically feasible.
         flattened_cloud = np.copy(cropped_cloud.xyzs())
         flattened_cloud[2,:] = np.clip(
             flattened_cloud[2,:],
             np.max(flattened_cloud[2,:]) - 0.001,
             np.max(flattened_cloud[2,:])
         )
-        principal_component, secondary_component, _ = compute_principal_minor_components(cropped_cloud.xyzs().T)
+        principal_component, secondary_component, _ = compute_principal_minor_components(flattened_cloud.T)
         
         # visualize axes, principal axis is z axis (blue), secondary axis is x axis (red)
         z_axis, x_axis = [0.0, 0.0, 1.0], [1.0, 0.0, 0.0]