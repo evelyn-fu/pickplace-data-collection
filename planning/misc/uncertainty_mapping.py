969e411db4ef65b12ac804376e6a633ec823ae50
Evelyn Fu <evelynfu15@gmail.com>
2025-04-20 16:53:24 -0400

    uncertainty mapping testing

--- planning/misc/uncertainty_mapping.py
+++ planning/misc/uncertainty_mapping.py
@@ -1,5 +1,6 @@
 import numpy as np
 import open3d as o3d
+from tqdm import tqdm
 from pydrake.all import (
     PointCloud,
     RigidTransform,
@@ -42,24 +43,27 @@ class VoxelMap:
         self.pcd.mutable_normals()[:] = -self.pcd.normals() # flip away from center
         self.normals = self.pcd.normals()
 
-        self.triangle_mesh, densities = o3d.geometry.TriangleMesh.create_from_point_cloud_poisson(
-            o3d.geometry.PointCloud(
-                o3d.utility.Vector3dVector(self.points.T)
-            )
+        o3d_pcd = o3d.geometry.PointCloud(
+            o3d.utility.Vector3dVector(self.points.T)
         )
+        o3d_pcd.normals = o3d.utility.Vector3dVector(self.normals.T)
+        triangle_mesh, densities = o3d.geometry.TriangleMesh.create_from_point_cloud_poisson(
+            o3d_pcd
+        )
+        tensor_mesh = o3d.t.geometry.TriangleMesh.from_legacy(triangle_mesh)
 
         self.scene = o3d.t.geometry.RaycastingScene()
-        self.scene.add_triangles(self.triangle_mesh)
+        self.scene.add_triangles(tensor_mesh)
 
-        print(f"Initialized voxel map with {len(self.voxel_map)} voxels.")
+        print(f"Initialized voxel map with {self.points.shape[1]} voxels.")
 
-    def update_with_observation(self, intrinsic_matrix, extrinsic_matrix, width_px, height_px, occlusion_mask=None):
+    def update_with_observation(self, intrinsic_matrix, extrinsic_matrix, width_px, height_px, occlusion_mask=None, visualize=False):
         """
         Updates the voxel map with a new observation.
 
         Args:
             intrinsic_matrix (np.ndarray): The intrinsic matrix of the camera
-            extrinsic_matrix (np.ndarray): The extrinsic matrix of the camera in object frame
+            extrinsic_matrix (np.ndarray): The extrinsic matrix of the camera
             width_px (int): The width of the camera image in pixels
             height_px (int): The height of the camera image in pixels
             occlusion_mask (np.ndarray): A 1xn array indicating occluded pixels, optional
@@ -67,7 +71,7 @@ class VoxelMap:
 
         rays = o3d.t.geometry.RaycastingScene.create_rays_pinhole(
             intrinsic_matrix=intrinsic_matrix,
-            extrinsic_matrix=extrinsic_matrix,
+            extrinsic_matrix=np.linalg.inv(extrinsic_matrix),
             width_px=width_px,
             height_px=height_px,
         )
@@ -77,35 +81,60 @@ class VoxelMap:
         if occlusion_mask is not None:
             occlusion_indices = np.where(occlusion_mask == 1)[0]
 
-        for i in range(rays.shape[0]):
-            ray = rays[i]
+        point_cloud = o3d.geometry.PointCloud()
+        point_cloud.points = o3d.utility.Vector3dVector(self.points.T)
+
+        if visualize:
+            self.visualize(X_cam=extrinsic_matrix)
+        
+        points = []
+        lines = []
+        print(f"ray origin: {rays[0][0][:3].numpy()}")
+        for idx in tqdm(range(rays.shape[0] * rays.shape[1]), desc="Casting rays"):
+            i = idx // rays.shape[1]
+            j = idx % rays.shape[1]
+            ray = rays[i, j]
             ray_dir = ray[3:6]
             ray_origin = ray[:3]
-            if raycast["t_hit"][i] < np.inf:
-                depth = raycast["t_hit"][i]
+            points += [ray_origin.numpy(), (ray_origin + ray_dir * 0.1).numpy()]
+            lines.append([len(points) - 2, len(points) - 1])
+            if raycast["t_hit"][i, j] < np.inf:
+                print(f"Hit at pixel ({i}, {j})")
+                depth = raycast["t_hit"][i, j]
                 hit_point = ray_origin + depth * ray_dir
-                nearest_voxel_index = np.argmin(np.linalg.norm(hit_point - self.points))
-                if nearest_voxel_index not in occlusion_indices:
+                nearest_voxel_index = np.argmin(np.linalg.norm(np.expand_dims(hit_point, axis=1) - self.points))
+                if occlusion_mask is not None and nearest_voxel_index not in occlusion_indices:
                     # Update signed distance and confidence
                     surface_normal = self.normals[nearest_voxel_index]
                     weight = -np.dot(surface_normal, ray_dir)
                     self.confidences[nearest_voxel_index] += weight
+        
+        line_set = o3d.geometry.LineSet()
+        line_set.points = o3d.utility.Vector3dVector(np.array(points))
+        line_set.lines = o3d.utility.Vector2iVector(lines)
+        o3d.visualization.draw_geometries([line_set, point_cloud])
     
-    def visualize(self):
+    def visualize(self, X_cam=None):
         """
         Visualizes the voxel map using open3d
         """
         point_cloud = o3d.geometry.PointCloud()
         point_cloud.points = o3d.utility.Vector3dVector(self.points.T)
 
-        base_color = np.array([0.2, 0.2, 0.2])
-        target_color = np.array([1, 0, 0])
-        colors = base_color + np.clip(self.confidences, 0, 1) * (target_color - base_color) # red more confident, gray less confident
+        base_color = np.array([[0.2, 0.2, 0.2]])
+        target_color = np.array([[1, 0, 0]])
+        colors = base_color + np.expand_dims(np.clip(self.confidences, 0, 1), axis=1) @ (target_color - base_color) # red more confident, gray less confident
         point_cloud.colors = o3d.utility.Vector3dVector(colors)
 
-        o3d.visualization.draw_geometries([point_cloud])
+        geometries = [point_cloud]
+        if X_cam is not None:
+            observation_direction = o3d.geometry.TriangleMesh.create_coordinate_frame(size=0.1)
+            observation_direction.transform(X_cam)
+            geometries.append(observation_direction)
 
-    def fully_obserrve(self):
+        o3d.visualization.draw_geometries(geometries)
+
+    def fully_observed(self):
         """
         Returns True if the map is fully observed
         """
